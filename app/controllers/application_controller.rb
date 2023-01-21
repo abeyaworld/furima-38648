fe4f92447d81de7f3a301197b94b9924d4268cc5
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-21 22:48:59 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'abeyaworld' && password == 'ef32134'
+    end
+  end
 end
