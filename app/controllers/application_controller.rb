e3673c8d4419d9773251fca506bf32002e6483c4
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-22 13:04:13 +0900

    applicationコントローラー設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permited_parameters, if: :devise_controller?
 
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'abeyaworld' && password == 'ef32134'
     end
+  end
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
   end
 end
