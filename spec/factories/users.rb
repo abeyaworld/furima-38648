4a655821df56ad568d5d8c6dfde6ccb3a8577e6e
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-28 13:59:05 +0900

    FactoryBot準備＋ファイル設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    transient do
+      person { Gimei.name }
+    end
+    nickname {Faker::Name.initials(number: 3)}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    encrypted_password {password}
+    family_name {person.last.kanji}
+    first_name {person.first.kanji}
+    family_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    birthday {Faker::Date.backward}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'テスト' do
-    it 'テスト２' do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる' do
+      it '全ての項目が存在すれば登録できる' do
+      end
+    end
+    context '新規登録できない' do
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it '同じemailがあったら登録できない' do
+      end
+      it 'emailに@がないと登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
+      it 'passwordが数字だけでは登録できない' do
+      end
+      it 'passwordが文字だけでは登録できない' do
+      end
+      it 'passwordが全角では登録できない' do
+      end
+      it 'family_nameが空では登録できない' do
+      end
+      it 'first_nameが空では登録できない' do
+      end
+      it 'family_nameが全角でないと登録できない' do
+      end
+      it 'first_nameが全角でないと登録できない' do
+      end
+      it 'family_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaが空では登録できない' do
+      end
+      it 'family_name_kanaがカタカナでないと登録できない' do
+      end
+      it 'first_name_kanaがカタカナでないと登録できない' do
+      end
+      it 'birthdayが空では登録できない' do
+      end
     end
   end
 end
