658811498b58c71e1949988ac152dc962a99f42a
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-28 16:29:25 +0900

    ユーザー管理機能修正3

--- db/migrate/20230122020624_devise_create_users.rb
+++ db/migrate/20230122020624_devise_create_users.rb
@@ -11,9 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name, null: false
       t.string :family_name_kana, null: false
       t.string :first_name_kana, null: false
-      t.integer :birth_year ,null: false
-      t.integer :birth_month, null: false
-      t.integer :birth_day, null: false
+      t.date :birthday ,null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname { Faker::Name.initials(number: 3) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     encrypted_password { password }
     family_name { person.last.kanji }
     first_name { person.first.kanji }
