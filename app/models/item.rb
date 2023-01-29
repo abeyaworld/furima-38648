11b86f191160697aa5c9e047fb77835fbb66b895
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-29 17:44:41 +0900

    バリテーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,9 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  VALID_PRICE = /\A[0-9]+\z/
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 999_999 }, format: { with: VALID_PRICE }
   validates :image, presence: true
+
 
 end
