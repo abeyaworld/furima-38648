0bf8c4a903320bcebf248b16ce1afa3fa2c3bc60
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-29 17:13:13 +0900

    Itemsバリテーション＋エラー表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,19 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :item_prefecture
   belongs_to :shipping_date
+  validates :category_id, numericality: { other_than: 1}
+  validates :condition_id, numericality: { other_than: 1}
+  validates :shipping_fee_id, numericality: { other_than: 1}
+  validates :item_prefecture_id, numericality: { other_than: 1}
+  validates :shipping_date_id, numericality: { other_than: 1}
+
   has_one_attached :image
 
   belongs_to :user
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
 end
