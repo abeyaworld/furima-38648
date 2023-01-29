9c41aa0b3c7798b63fc30130b48ee73e388516d4
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-29 13:46:41 +0900

    出品画面への遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+  end
+
+  def new
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
+
+  resources :items, only: [:index, :new]
 end
