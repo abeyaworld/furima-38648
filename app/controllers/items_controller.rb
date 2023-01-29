9032b66207f9cf9d4d37f6f226cecb95ce4e70be
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-29 15:33:57 +0900

    投稿内容の保存＋画像の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :item_prefecture_id, :shipping_date_id, :user, :image)
   end
 end
