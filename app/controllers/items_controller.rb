859ea18499ab6ae1e0927c8deb908fdb9beff1a8
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-01-30 20:49:02 +0900

    商品詳細の表示内容整備2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
