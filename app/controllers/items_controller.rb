836990b426d51e6d79549f02b94c10378c03e6f5
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-02-03 20:07:34 +0900

    商品情報編集の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
@@ -39,7 +42,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     return if user_signed_in?
-
     redirect_to new_user_session_path
   end
 
