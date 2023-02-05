7173017e14f4c19534ed954d528fee9309e3447c
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-02-05 17:16:19 +0900

    Fromオブジェクト利用による複数テーブル登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:post_num, :item_prefecture_id, :municipality, :house_num, :building, :phone_num, :purchase_record).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,24 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_num, :item_prefecture_id, :municipality, :house_num, :building, :phone_num, :purchase_record, :order_id
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # paymentモデルのバリデーション
+    validates :post_num, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: ' が不正です。ハイフン(-)を含めてください' }
+    validates :item_prefecture_id, numericality: { other_than: 0, message: "を選択してください" }
+    validates :municipality
+    validates :house_num
+    validates :phone_num, format: { with: /\A[0-9]{10,11}\z/, message: 'が不正です' }
+    # トークンのバリデーション
+    # validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_num: post_num, item_prefecture_id: item_prefecture_id, municipality: municipality, house_num: house_num, building: building, phone_num: phone_num, order_id: order.id)
+  end
+
+end
