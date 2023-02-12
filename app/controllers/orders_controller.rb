75f669fdd057850ae7815377e12890e614643896
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-02-12 15:43:57 +0900

    画面遷移制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :item_params, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -21,6 +20,11 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_address).permit(:post_num, :item_prefecture_id, :municipality, :house_num, :building, :phone_num, :purchase_record).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def item_params
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
   def pay_item
