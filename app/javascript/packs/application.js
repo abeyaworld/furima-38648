d42bdf00ae666ca297f81ca0dc1e35efd9338538
abeyaworld <118046008+abeyaworld@users.noreply.github.com>
2023-02-12 20:12:38 +0900

    Revert "商品購入機能"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../new")
-require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
